8a8eb3cfa0790310c593d2c4c360b8f1388f0547
sven4500 <sven4500@mail.ru>
2020-03-24 00:10:12 +0300

    Добавлен класс view model для главного окна.

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -15,11 +15,12 @@ using System.Windows.Shapes;
 
 namespace Course
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        // TODO: хздесь немного криво получается потому что нужно править класс
+        // view model в двух местах. Здесь и в XAML схеме.
+        private MainWindowVM ViewModel { get { return DataContext as MainWindowVM; } }
+
         public MainWindow()
         {
             InitializeComponent();
---
+++ MainWindowVM.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Course
+{
+    public class MainWindowVM
+    {
+        public static readonly uint maxDim = 1000;
+
+        public uint BugCountTotal { get { return BugCountRed + BugCountBlue; } }
+        public uint MapSize { get { return MapWidth * MapHeight; } }
+
+        private uint mapWidth = 200;
+        public uint MapWidth { get { return mapWidth; } set { mapWidth = (value < maxDim && value * MapHeight > BugCountTotal) ? value : maxDim; } }
+
+        private uint mapHeight = 100;
+        public uint MapHeight { get { return mapHeight; } set { mapHeight = (value < maxDim && value * MapHeight > BugCountTotal) ? value : maxDim; } }
+
+        private uint feedRate = 0;
+        public uint FeedRate { get { return feedRate; } set { feedRate = value; } }
+
+        // Далее следует описание настроек обоих команд.
+        private uint bugCountBlue = 10;
+        public uint BugCountBlue { get { return bugCountBlue; } set { bugCountBlue = (value + BugCountRed < MapSize) ? value : bugCountBlue; } }
+
+        private uint bugCountRed = 10;
+        public uint BugCountRed { get { return bugCountRed; } set { bugCountRed = (value + BugCountBlue < MapSize) ? value : bugCountRed; } }
+
+        private uint bugHealthBlue = 100;
+        public uint BugHealthBlue { get { return bugHealthBlue; } set { bugHealthBlue = (value > 0) ? value : bugHealthBlue; } }
+
+        private uint bugHealthRed = 100;
+        public uint BugHealthRed { get { return bugHealthRed; } set { bugHealthRed = (value > 0) ? value : bugHealthRed; } }
+
+        private uint bugSpeedBlue = 1;
+        public uint BugSpeedBlue { get { return bugSpeedBlue; } set { bugSpeedBlue = value; } }
+
+        private uint bugSpeedRed = 1;
+        public uint BugSpeedRed { get { return bugSpeedRed; } set { bugSpeedRed = value; } }
+
+        private uint bugEatRateBlue = 1;
+        public uint BugEatRateBlue { get { return bugEatRateBlue; } set { bugEatRateBlue = value; } }
+
+        private uint bugEatRateRed = 1;
+        public uint BugEatRateRed { get { return bugEatRateRed; } set { bugEatRateRed = value; } }
+
+        private uint bugStrengthBlue = 3;
+        public uint BugStrengthBlue { get { return bugStrengthBlue; } set { bugStrengthBlue = value; } }
+
+        private uint bugStrengthRed = 3;
+        public uint BugStrengthRed { get { return bugStrengthRed; } set { bugStrengthRed = value; } }
+
+    }
+}
